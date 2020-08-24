f38792780a5d2800d0e44b16bf168601f8ca654d
tplatt <theop@nmsu.edu>
2020-08-24 14:21:16 -0600

    Develope components of lab 1. Includes updated README.md. Touch'es necessary files, adds Hello World.

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.nmsu.cs.helloworld;
+
+/**
+ * @author Hamnp
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
